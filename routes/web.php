7a07945f23f16819a205b948806444a6ace26f7d
yasir-naseer <Qwerty_619>
2022-11-19 11:34:50 +0500

    Making app live

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 
 Route::group(['middleware' => ['verify.shopify'] ], function () {
 
