79428d1c49c319f1464f582f56a8764dc5729d16
yasir-naseer <Qwerty_619>
2022-12-17 00:38:47 +0500

    Testing app changes

--- app/Http/Controllers/AdminDashboardController.php
+++ app/Http/Controllers/AdminDashboardController.php
@@ -82,7 +82,7 @@ class AdminDashboardController extends Controller
 
             $data = [
                 "price_rule" => [
-                    "title" => $settings->product_discount .' Percent OFF',
+                    "title" => $settings->product_discount .' PERCENT OFF',
                     "target_type" => "line_item",
                     "target_selection" => "entitled",
                     "allocation_method" => "each",
@@ -120,7 +120,7 @@ class AdminDashboardController extends Controller
         else {
             $data = [
                 "price_rule" => [
-                    "title" => $settings->product_discount .' Percent OFF',
+                    "title" => $settings->product_discount .' PERCENT OFF',
                     "value_type" => "percentage",
                     "value" => '-' . $settings->product_discount,
                     'starts_at' => now(),
