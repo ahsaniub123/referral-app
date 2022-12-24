71a9e9847208530f0614ded8a3187491de7fb745
Muhammad Ahsan <76003371+ahsaniub123@users.noreply.github.com>
2022-12-24 15:49:11 +0500

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,7 +4,7 @@
         <div class="row align-items-center">
             <div class="col">
                 <h2 class="page-title">
-                    Dashboard
+                    Dashboard | Home
                 </h2>
             </div>
         </div>
