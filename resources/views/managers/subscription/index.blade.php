0c321f9e9d772026aaa0aa7fb88e2db410caa84b
Muhammad Ahsan <76003371+ahsaniub123@users.noreply.github.com>
2023-01-17 23:13:04 +0500

    Update index.blade.php

--- resources/views/managers/subscription/index.blade.php
+++ resources/views/managers/subscription/index.blade.php
@@ -1,2 +1,15 @@
 
-<p>This is Page</p>
+@extends('layouts.admin')
+
+@section('header')
+    <div class="page-header d-print-none">
+        <div class="row align-items-center">
+            <div class="col">
+                <h2 class="page-title">
+                    Subscribe a plan
+                </h2>
+            </div>
+        </div>
+    </div>
+@endsection
+
