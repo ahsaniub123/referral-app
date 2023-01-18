e274eed3f279260e19fc798bca90f2470961890a
Muhammad Ahsan <76003371+ahsaniub123@users.noreply.github.com>
2023-01-18 22:41:13 +0500

    Update dashboard.blade.php

--- resources/views/managers/dashboard.blade.php
+++ resources/views/managers/dashboard.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin')
-
+<script src="https://code.jquery.com/jquery-3.6.3.min.js" integrity="sha256-pvPw+upLPUjgMXY0G+8O0xUf+/Im1MZjXxxgOcBQBXU=" crossorigin="anonymous"></script>
 @section('styles')
     <style>
         /**
